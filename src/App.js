1d9ca50052321a60d2beb7afbcfc440edddd2e45
Ivan Samozhenov <i_dont_known@mail.ru>
2024-04-09 11:42:14 +0300

    Add deps, initiate structure of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import {
+  Route, BrowserRouter as Router, Routes,
+} from 'react-router-dom';
 
+import TOKEN from './TOKEN';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Router>
+        <Routes>
+          <Route
+            element={(
+              <div>
+                <h1>Hello World</h1>
+              </div>
+            )}
+            path="/page2"
+          />
+          <Route
+            element={(
+              <div>
+                <a href="/page2">
+                  туда
+                </a>
+              </div>
+            )}
+            path="/"
+          />
+        </Routes>
+      </Router>
+    </header>
+  </div>
+);
 
 export default App;
